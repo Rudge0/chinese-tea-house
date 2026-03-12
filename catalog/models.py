dec2038c358c81838e2a465b1c1d79d7943d78b1
Maksym Pariichuk <maksim.pariychuk07@gmail.com>
2026-03-12 15:14:57 +0100

    fix: (typo) Add '?' in confirm delete, Add MinValueValidator in models for price

--- catalog/models.py
+++ catalog/models.py
@@ -38,7 +38,7 @@ class Supplier(AbstractUser):
 
 class Tea(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     harvest_year = models.IntegerField(validators=[
         MinValueValidator(0),
         max_current_year
